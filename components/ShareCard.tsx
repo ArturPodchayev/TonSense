af739a2d6d2767971ccae1385589e8d9d8272b5e
ArturPodchayev <arturpodchaev@gmail.com>
2026-04-20 03:31:08 +0500

    change the link a bit

--- components/ShareCard.tsx
+++ components/ShareCard.tsx
@@ -45,7 +45,7 @@ const ShareCard = forwardRef<HTMLDivElement, Props>(
             </span>
             <span style={{ width: 7, height: 7, borderRadius: "50%", background: "#0098EA", display: "inline-block", marginTop: 2 }} />
           </div>
-          <span style={{ fontSize: 12, color: "#6B7280" }}>tonsense.vercel.app</span>
+          <span style={{ fontSize: 12, color: "#6B7280" }}>ton-sense.vercel.app</span>
         </div>
 
         {/* Headline */}
@@ -95,7 +95,7 @@ const ShareCard = forwardRef<HTMLDivElement, Props>(
             borderRadius: 8,
             padding: "4px 12px",
           }}>
-            tonsense.vercel.app
+            ton-sense.vercel.app
           </span>
         </div>
       </div>
